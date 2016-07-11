9420f8cabab820768c4cb282744cbaca77ffc48b
Ferran Caellas <ferran.caellas@teamcmp.com>
2016-07-11 17:55:35 +0200

    WHN-180 Remove duplicated logger

--- src/Cmp/DomainEvent/Infrastructure/Publisher/RabbitMQ/RabbitMQPublisherFactory.php
+++ src/Cmp/DomainEvent/Infrastructure/Publisher/RabbitMQ/RabbitMQPublisherFactory.php
@@ -20,7 +20,6 @@ class RabbitMQPublisherFactory
     public function create($config)
     {
         $this->logger->info('Using RabbitMQ Publisher');
-        $this->logger->info(sprintf('Connecting to RabbitMQ, Host: %s, Port: %s, User: %s, Exchange: %s', $config['host'], $config['port'], $config['user'], $config['exchange']));
         $amqpLazyConnection = new AMQPLazyConnection($config['host'], $config['port'], $config['user'], $config['password']);
         $rabbitMQPublisherInitializer = new RabbitMQPublisherInitializer($amqpLazyConnection, $config, $this->logger);
         return new RabbitMQPublisher($rabbitMQPublisherInitializer, $config, $this->logger);
